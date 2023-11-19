abf1b984b9cf4d85911a31c7bbe8f4f08879d2f6
Sharafudeen jafani Sharafudeen <esjafani@gmail.com>
2023-11-19 17:56:15 +0530

    Q4

---
+++ Q4.js
@@ -0,0 +1,12 @@
+const readlineSync=require('readline-sync');
+console.log("YOU CAN FIND ODD NUMBERS SUM ");
+let sum=0;
+let limit=parseInt(readlineSync.question("Enter A limit to find Odd numbers sum"));
+for (let i=1;i<=limit;i++){
+    
+    if(i % 2==!0){
+     
+        sum+=i
+    }
+}
+console.log(sum);
