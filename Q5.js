9daad395e1518eb24d20b33c54f55c9c9be7ed75
Sharafudeen jafani Sharafudeen <esjafani@gmail.com>
2023-11-19 22:26:43 +0530

    img

--- Q5.js
+++ Q5.js
@@ -6,15 +6,16 @@ let even=[];
 for(let i=0;i<number;i++){
     array[i]=parseInt(readlineSync.question('enter' +(i+1)+'th Element:'));
     if(array[i]%2==0){
-        
+        even=array[i];
     }
 }
 for(let c=0;c<number;c++){
     if(array[c] %2 ==0){
-
+        
     count++;
 
 
     }
     
 }console.log("Number of even numbers in the given array is",count);
+console.log("the Even numbers are:",even)
